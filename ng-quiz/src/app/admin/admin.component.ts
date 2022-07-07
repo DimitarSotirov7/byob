c189d662d4810bb3402095d419475f2b84fc4165
Dimitar <Dimitar>
2022-07-07 17:24:05 +0300

    Create admin category creation

--- ng-quiz/src/app/admin/admin.component.ts
+++ ng-quiz/src/app/admin/admin.component.ts
@@ -13,23 +13,47 @@ import { AuthService } from '../services/auth/auth.service';
 })
 export class AdminComponent {
 
+  categories: { name: string, id: string, selected: boolean }[] | undefined;
+  categToggle: boolean = true; //New
+
   constructor(
     private authService: AuthService,
     private route: ActivatedRoute,
     private quizService: QuizService,
     private categoryService: CategoryService,
     private questionService: QuestionService
-  ) { }
+  ) {
+    this.loadCategories();
+  }
 
-  submit(input: any) {
-    console.log(input);
-
+  add(input: any) {
     if (input?.category) {
-      this.categoryService.add({ name: input.category } as ICategoryModel).then(res => {
-        if (res.id) {
-          this.authService.authMsg.emit('Category has been added successfully!');
-        }
-      });
+      const name = this.capitalize(input.category);
+      const categoryExists = this.categories?.some(c => c.name === name);
+      if (!categoryExists) {
+        this.categoryService.add({ name } as ICategoryModel).then(data => {
+          if (data.id) {
+            this.authService.authMsg.emit('Category has been added successfully!');
+          }
+        });
+      } else {
+        this.authService.authMsg.emit('Category exists!');
+      }
     }
+  }
+
+  loadCategories() {
+    this.categoryService.getAll().get().subscribe(res => {
+      this.categories = res.docs.map(c => ({ ...c.data(), id: c.id })) as { name: string, id: string, selected: boolean }[];
+    });
+  }
+
+  capitalize(text: string): string {
+    if (text.length < 1) {
+      return text;
+    }
+
+    const firstChar = text.charAt(0).toUpperCase();
+    return firstChar + text.slice(1);
   }
 }
