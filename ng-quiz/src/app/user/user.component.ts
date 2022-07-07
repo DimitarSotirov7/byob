32ee3c6bfa599c97c9f559b8e59ec8d17dd6141d
Dimitar <Dimitar>
2022-07-07 12:47:07 +0300

    Create User funcs

--- ng-quiz/src/app/user/user.component.ts
+++ ng-quiz/src/app/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IFormModel } from '../interfaces/form-model';
 import { AuthService } from '../services/auth/auth.service';
 
 @Component({
@@ -8,9 +9,50 @@ import { AuthService } from '../services/auth/auth.service';
 })
 export class UserComponent {
 
-  constructor(private authService: AuthService) { }
+  authState = this.authService.authState;
+  serverError: string | undefined;
+  cookieName: string = 'b%7y@80*6b9!';
+  logged: boolean = document.cookie.includes(this.cookieName);
+  formTitle: string = 'Sign Up';
 
-  submit(input: { username: string, password: string }) {
-    console.log(input)
+  constructor(private authService: AuthService) {
+    console.log(this.authState)
+  }
+
+  submit(input: IFormModel) {
+
+    const cookies = document.cookie;
+    console.log(cookies)
+    if (this.logged) {
+      return;
+    }
+
+    this.register(input);
+  }
+
+  register(input: IFormModel) {
+    this.authService.register(input)
+      .then(res => {
+        this.formTitle = "Sign In";
+      })
+      .catch(err => {
+        this.serverError = err.message;
+      console.log(err);
+    });
+  }
+
+  login(input: IFormModel) {
+    this.authService.login(input)
+      .then(res => {
+        document.cookie = `${this.cookieName}=${res.user.uid}`;
+      })
+      .catch(err => {
+        this.serverError = err.message;
+      console.log(err);
+    });
+  }
+
+  logout() {
+    
   }
 }
