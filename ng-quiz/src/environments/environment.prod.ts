76db4f4cc202168ce288a9e36249d6e0020fed34
Dimitar <Dimitar>
2022-07-20 14:54:57 +0300

    Remove firebase

--- ng-quiz/src/environments/environment.prod.ts
+++ ng-quiz/src/environments/environment.prod.ts
@@ -1,17 +1,17 @@
 export const environment = {
   production: true,
-  appName: 'Quizzing',
+  appName: 'Quizo',
   firebase: {
     config: {
-      apiKey: "AIzaSyCFeO2c3-jy-1VAiDDE2v8vIk_4p92Tqv0",
-      authDomain: "ng-quiz-7.firebaseapp.com",
-      projectId: "ng-quiz-7",
-      storageBucket: "ng-quiz-7.appspot.com",
-      messagingSenderId: "280971311759",
-      appId: "1:280971311759:web:fc7370c3aa261925c00efa",
-      measurementId: "G-R183RV2JEY"
+      apiKey: "AIzaSyBHxqW_DP0Rsi2HMSSmNUaR2JhkZEpS9qQ",
+      authDomain: "quizgame-2d57e.firebaseapp.com",
+      projectId: "quizgame-2d57e",
+      storageBucket: "quizgame-2d57e.appspot.com",
+      messagingSenderId: "949475743242",
+      appId: "1:949475743242:web:563fdfe774daf7a20c31f3",
+      measurementId: "G-Z4BVZQZ8ME"
     },
-    dbUrl: 'https://ng-quiz-7-default-rtdb.europe-west1.firebasedatabase.app/',
+    dbUrl: 'https://quizgame-default-rtdb.europe-central2.firebasedatabase.app/',
   },
   admin: {
     email: 'dimitarsotirov7@gmail.com'
