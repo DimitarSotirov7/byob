7698a2bb8ae4985057340e2d8e893115a43d3845
Dimitar <Dimitar>
2022-08-11 18:11:24 +0300

    Add old server

--- ng-quiz/src/environments/environment.prod.ts
+++ ng-quiz/src/environments/environment.prod.ts
@@ -3,16 +3,20 @@ export const environment = {
   appName: 'Quizo',
   firebase: {
     config: {
-      apiKey: "AIzaSyBHxqW_DP0Rsi2HMSSmNUaR2JhkZEpS9qQ",
-      authDomain: "quizo.firebaseapp.com",
-      projectId: "quizo",
-      storageBucket: "quizo.appspot.com",
+      // apiKey: "AIzaSyBHxqW_DP0Rsi2HMSSmNUaR2JhkZEpS9qQ",
       // authDomain: "quizgame-2d57e.firebaseapp.com",
       // projectId: "quizgame-2d57e",
       // storageBucket: "quizgame-2d57e.appspot.com",
-      messagingSenderId: "949475743242",
-      appId: "1:949475743242:web:563fdfe774daf7a20c31f3",
-      measurementId: "G-Z4BVZQZ8ME"
+      // messagingSenderId: "949475743242",
+      // appId: "1:949475743242:web:563fdfe774daf7a20c31f3",
+      // measurementId: "G-Z4BVZQZ8ME"
+      apiKey: "AIzaSyCFeO2c3-jy-1VAiDDE2v8vIk_4p92Tqv0",
+      authDomain: "ng-quiz-7.firebaseapp.com",
+      projectId: "ng-quiz-7",
+      storageBucket: "ng-quiz-7.appspot.com",
+      messagingSenderId: "280971311759",
+      appId: "1:280971311759:web:fc7370c3aa261925c00efa",
+      measurementId: "G-R183RV2JEY"
     },
   },
   admin: {
